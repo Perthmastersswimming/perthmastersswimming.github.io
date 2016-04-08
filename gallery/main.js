8d66593df13938df55a4495de1a6982fbeec7f49
Perthmastersswimming <perthmastersswimming@hotmail.com>
2016-04-08 17:20:42 +0100

    Update main.js

--- gallery/main.js
+++ gallery/main.js
@@ -1,22 +1,14 @@
-/* 
- _____   _           _         _                        _                  
-|_   _| | |         | |       | |                      | |                 
-  | |   | |__   __ _| |_ ___  | |_ ___  _ __ ___   __ _| |_ ___   ___  ___ 
-  | |   | '_ \ / _` | __/ _ \ | __/ _ \| '_ ` _ \ / _` | __/ _ \ / _ \/ __|
- _| |_  | | | | (_| | ||  __/ | || (_) | | | | | | (_| | || (_) |  __/\__ \
- \___/  |_| |_|\__,_|\__\___|  \__\___/|_| |_| |_|\__,_|\__\___/ \___||___/
+var img = document.querySelector('img')
 
-Oh nice, welcome to the js file of dreams.
-Enjoy responsibly!
-@ihatetomatoes
+function loaded() {
+  alert('loaded')
+}
 
-*/
-
-$(document).ready(function() {
-	
-	setTimeout(function(){
-		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
-	}, 3000);
-	
-});
+if (img.complete) {
+  loaded()
+} else {
+  img.addEventListener('load', loaded)
+  img.addEventListener('error', function() {
+      alert('error')
+  })
+}
