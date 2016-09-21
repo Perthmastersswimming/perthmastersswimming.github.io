6cb8a3c1fb06044dec20c27e2cb198a16f4309b5
Perthmastersswimming <perthmastersswimming@hotmail.com>
2016-09-21 12:38:34 +0100

    Update index1.js

--- about/index1.js
+++ about/index1.js
@@ -151,3 +151,23 @@ $(function(){
     })
   })
 }).call(this);
+
+// google map 
+function initialize() {
+            var mapOptions = {
+                center: new google.maps.LatLng(56.3947167, -3.4448415),
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.HYBRID,
+                scrollwheel: true,
+                draggable: true,
+                panControl: true,
+                zoomControl: true,
+                mapTypeControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                overviewMapControl: true,
+                rotateControl: true,
+            };
+            var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
